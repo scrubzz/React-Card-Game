2ebe9e5f1653f64e9c6434745f6a34014584fa41
Princeton Baker <princeton.bake10@gmail.com>
2020-06-13 14:34:06 -0400

    complete React App

---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './MemoryCard.css';
+import logo from './dclogo.jpg';
+
+
+export default class Card extends Component {
+    render() {
+        let innerClass = "MemoryCard__inner";
+        if (this.props.isFlipped) {
+            innerClass += ' flipped';
+        }
+        return (
+            <div className="MemoryCard" onClick={this.props.pickCard}>
+                <div className={innerClass}>
+                    <div className="MemoryCard__back">
+                        <img src={logo} alt="" />
+                    </div>
+                    <div className="MemoryCard__front">
+                        {this.props.symbol}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
